0936f84be77d45c2ba78f89437f9f95753939434
dvladimir95 <81610217+dvladimir96@users.noreply.github.com>
2021-05-07 20:45:44 -0500

    Create hola mundo.py

---
+++ hola mundo.py
@@ -0,0 +1,22 @@
+print("hola mundo")
+
+name = input("cual es tu nombre?")
+print("hola", name)
+print("hola"+ name)
+
+x = ((3+2) / (2*5))**2
+print(x)
+
+
+horas = int(input("horas trabajadas"))
+costo = int(input("costo por hora"))
+paga = (horas * costo)
+print("la paga es", paga) 
+
+n = int(input("valor"))
+s = (n*(n + 1))*0.5
+
+
+nombre = input("cual es tu nombre")
+numero = input("ingrese el numero")
+print(nomre * int(numero))
